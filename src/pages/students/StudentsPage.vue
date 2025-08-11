0878a886d57e1ab9b95a7b89c4fd971d9e1b529a
Wendellynizer <wendelsabelo27@gmail.com>
2025-08-12 00:35:43 +0800

    added custom datatables

--- src/pages/students/StudentsPage.vue
+++ src/pages/students/StudentsPage.vue
@@ -1,7 +1,28 @@
-<script setup>
+<script setup lang="ts">
+import Container from '@components/common/Container.vue';
+import DataTable from '@components/tables/DataTable.vue';
 
+const columns = [
+    {name: 'student_id', title: 'Student ID'},
+    {name: 'name', title: 'Fullname'},
+    {name: 'name', title: 'Program/Year/Set'},
+    {name: 'name', title: 'Status'},
+]
+
+const testData = [
+    {id: 1, student_id: '2001-00012', fullname: 'Ricardo Marimar', set: 'BSIT 4A', status: 'ENROLLED'},
+    {id: 2, student_id: '2001-00012', fullname: 'Ricardo Marimar', set: 'BSIT 4A', status: 'ENROLLED'},
+    {id: 3, student_id: '2001-00012', fullname: 'Ricardo Marimar', set: 'BSIT 4A', status: 'ENROLLED'},
+    {id: 4, student_id: '2001-00012', fullname: 'Ricardo Marimar', set: 'BSIT 4A', status: 'ENROLLED'},
+    {id: 5, student_id: '2001-00012', fullname: 'Ricardo Marimar', set: 'BSIT 4A', status: 'ENROLLED'},
+]
 </script>
 
 <template>
-    <p>Students</p>
+    <Container>
+        <p class="text-xl font-medium mb-4">Students</p>
+
+        <DataTable :columns="columns" :data="testData" />
+    </Container>
+    
 </template>
