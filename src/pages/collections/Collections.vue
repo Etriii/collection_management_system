1e4e738156aafce0d91210da5dc0c813a5be6943
drennn <mamalealdren@gmail.com>
2026-02-26 21:22:02 +0800

    added CollectionCard import statement and updated the path to the component in Collections.vue

--- src/pages/collections/Collections.vue
+++ src/pages/collections/Collections.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref, computed} from "vue"
-import CollectionCard from "@components/card/CollectionCard.vue"
+import CollectionCard from "./components/card/CollectionCard.vue"
 import {
   Plus,
   Search,
